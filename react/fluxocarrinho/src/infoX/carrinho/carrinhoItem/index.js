2296a1eac45639cc3cae4a0ba293eaa23a08676e
anaclara <anaclara.sw@hotmail.com>
2021-10-07 18:22:45 -0300

    dessa vez vaiii

--- react/fluxocarrinho/src/infoX/carrinho/carrinhoItem/index.js
+++ react/fluxocarrinho/src/infoX/carrinho/carrinhoItem/index.js
@@ -3,3 +3,33 @@ import { useState } from 'react';
 
 import Contador from '../contador'
 import { Container, RemoverIcon} from './styled'
+
+
+export default function carrinhoItem(props) {
+    const [produto, setProduto] = useState(props.info);
+
+    function alterarQtd(qtd) {
+        setProduto({...produto, qtd});
+
+        props.onUpdate(produto.id, qtd)
+    }
+
+    function remover() {
+        props.onRemove(produto.id);
+    }
+
+     return(
+         <div>
+             
+         </div>
+     )
+
+
+
+
+
+
+
+
+
+}
